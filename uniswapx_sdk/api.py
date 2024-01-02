ebb9d03ba95af94927695b3781521bfb761d35ae
Elnaril <elnaril@gmx.com>
2024-01-02 15:30:11 +0100

    add api

---
+++ uniswapx_sdk/api.py
@@ -0,0 +1,40 @@
+from typing import (
+    Any,
+    Dict,
+    Optional,
+)
+from aiohttp import ClientSession
+
+from uniswapx_sdk.constants import uniswapx_orders_endpoint
+
+
+class UniswapXAPI:
+    def __init__(self, chain_id: int) -> None:
+        self.chain_id = chain_id
+
+    @staticmethod
+    async def _get_orders(session: ClientSession, **params: str) -> Dict[str, Any]:
+        async with session.get(url=uniswapx_orders_endpoint, params=params) as response:
+            return await response.json()
+
+    async def get_orders(
+            self,
+            order_status: str = "open",
+            limit: int = 10,
+            session: Optional[ClientSession] = None,
+            **kwargs: str) -> Dict[str, Any]:
+        """
+        Get orders from UniswapX API (https://api.uniswap.org/v2/orders)
+        :param order_status: one of the following str: open, expired, error, cancelled, filled, insufficient-funds
+        :param limit: result max count
+        :param session: optional. A valid aiohttp.ClientSession instance
+        :param kwargs: other possible parameters as describe here: https://api.uniswap.org/v2/uniswapx/docs
+        :return: The list of corresponding orders
+        """
+        params = kwargs.copy()
+        params.update({"chainId": self.chain_id, "orderStatus": order_status, "limit": limit})
+        if session:
+            return await self._get_orders(session, **params)
+        else:
+            async with ClientSession() as client_session:
+                return await self._get_orders(client_session, **params)
